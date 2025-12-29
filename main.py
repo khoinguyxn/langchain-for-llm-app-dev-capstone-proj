91ed0eaa6a933ba8f2634bfadb340ce063dcd7a9
Khoi Nguyen <khoinm2003@gmail.com>
2025-12-29 16:39:40 +0700

    Feat: Add academia-mcp for extensive toolings

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import asyncio
+
 from research_chain import create_research_chain
 
 
-def main():
-    chain = create_research_chain()
+async def main():
+    chain = await create_research_chain()
 
     while True:
         question = input("Enter your research question (or 'exit' to quit): ")
@@ -10,10 +12,10 @@ def main():
         if question.lower() == "exit":
             break
 
-        response = chain.invoke(question)
+        response = await chain.ainvoke(question)
 
         print(f"Response: {response}\n")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- research_chain.py
+++ research_chain.py
@@ -1,30 +1,47 @@
 """Research Agent - Mono-agent with multiple search and research tools."""
 
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableSerializable
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_ollama import ChatOllama
 
 from chroma import create_chroma_client
 from models.research_answer import ResearchAnswer
 
 
-def create_research_chain() -> RunnableSerializable:
+async def create_research_chain() -> RunnableSerializable:
     """
     Create a research chain that uses Chroma vector store and Ollama chat model.
 
     Returns:
         Configured research chain as a RunnableSerializable.
     """
+    mcp_clients = MultiServerMCPClient(
+        {
+            "academia": {
+                "transport": "http",
+                "url": "http://localhost:5056/mcp",
+            }
+        }
+    )
+
+    mcp_tools = await mcp_clients.get_tools()
+
     model = ChatOllama(
         model="deepseek-r1:latest",
         temperature=0,
         max_tokens=1024,
         timeout=None,
         max_retries=2,
-    ).with_structured_output(ResearchAnswer)
+    ).bind_tools(mcp_tools)
 
     prompt = ChatPromptTemplate.from_template(
-        """You are a research assistant with access to PDF research pappers for in-depth summarization.
+        """You are a research assistant with access to PDF research papers for in-depth summarization.
+        
+        You have access to academia research tools that can:
+        - arxiv_search: Query arXiv with field-specific queries and filters.
+        - arxiv_download: Fetch a paper by ID and convert to structured text (HTML/PDF modes).
         
         Always provide clear, concise summary.
         
@@ -43,4 +60,5 @@ def create_research_chain() -> RunnableSerializable:
         }
         | prompt
         | model
+        | StrOutputParser()
     )
